9525b009cf4b59f0757c19fb98f42f18d30aa81e
Paul <paul.piauger2@gmail.com>
2024-03-04 09:51:44 +0100

    affichage nom class

--- src/app/pages/rendu-api/rendu-api.component.ts
+++ src/app/pages/rendu-api/rendu-api.component.ts
@@ -1,17 +1,20 @@
 import { Component} from '@angular/core';
 import { RecupapiService } from '../../services/recup-api.service';
 import {Iclasses} from '../../interfaces/iclasses'
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-renduapi',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor
+  ],
   templateUrl: './rendu-api.component.html',
   styleUrl: './rendu-api.component.scss'
 })
 export class RenduapiComponent{
 
-  resultClasse: Object = {};
+  resultClasse: Iclasses[] = [];
 
   constructor(private recupapiService: RecupapiService) { }
   ngOnInit() {
@@ -22,6 +25,7 @@ export class RenduapiComponent{
 
     this.recupapiService.getAllClasses().subscribe({
       next : (data) => {
+        console.log(data);
         this.resultClasse = data;
       },
       error : (err) => {
